997ba1cf812107d040b97f57a92789fb3d1ceae8
Rhys <rhysjlindsay@gmail.com>
2018-05-08 20:25:19 +0930

    fixed projectile shooting angle

--- Assets/_scripts/_guns/ProjectileGun.cs
+++ Assets/_scripts/_guns/ProjectileGun.cs
@@ -13,18 +13,12 @@ public class ProjectileGun : Gun
     {
         base.Fire();
 
-        Vector3 lookPoint;
         Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
-            lookPoint = hit.point;
-        else lookPoint = ray.GetPoint(30);
+        Vector3 lookPoint = ray.GetPoint(30);
 
         foreach (Transform origin in shotOrigins)
         {
-            GameObject proj = Instantiate(shotPrefab, origin.position, Quaternion.identity);
-            proj.transform.LookAt(lookPoint);
+            GameObject proj = Instantiate(shotPrefab, origin.position, Quaternion.LookRotation(lookPoint - transform.position));
             proj.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shotForce);
         }
     }
